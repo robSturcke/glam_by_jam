a0b2be5d9403bd757fa951d1aaa4424ef67bd4ac
Rob Sturcke <rob@rslabs.dev>
2020-10-04 09:51:01 -0400

    makes logo clickable to home

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,16 @@ import React from "react"
 import PropTypes from "prop-types"
 import Logo from "./logo"
 import Navigation from "./nav"
+import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <header>
     <div className="nav_wrap">
       <div className="text-center">
         <div className="logo">
-          <Logo alt={siteTitle} />
+          <Link to="/">
+            <Logo alt={siteTitle} />
+          </Link>
         </div>
       </div>
       <Navigation />
