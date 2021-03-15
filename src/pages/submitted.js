f8b90bdb344682fff96622830dbdb70e3d493122
Rob Sturcke <rob@rslabs.dev>
2021-03-14 22:30:58 -0400

    adds image for success on submission page

--- src/pages/submitted.js
+++ src/pages/submitted.js
@@ -1,20 +1,28 @@
 import { Link } from "gatsby"
 import React from "react"
 import SEO from "../components/seo"
+import SubmittedText from "../images/submitted_text.svg"
 
 const Submitted = () => (
   <>
     <SEO title="Success! Your submission has been sent!" />
     <div className="primary_wrap">
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <div className="text-center py-4">
-              <h1>Success!</h1>
-              <h1>Your submission has been sent!</h1>
-              <h4>
-                <Link to="/">Back to Home</Link>
-              </h4>
+        <div className="content_wrap">
+          <div className="row">
+            <div className="col-12">
+              <div className="text-center py-4">
+                <img
+                  src={SubmittedText}
+                  width="450"
+                  className="img-fluid"
+                  alt="Submitted!"
+                />
+                <h1>Your submission has been sent!</h1>
+                <h4>
+                  <Link to="/">Back to Home</Link>
+                </h4>
+              </div>
             </div>
           </div>
         </div>
