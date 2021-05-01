ddbc048813f3ca111ba1df3020bfefce0c400830
Rob Sturcke <rob@rslabs.dev>
2021-05-01 12:33:49 -0400

    fixes use-instagram

--- src/hooks/use-instagram.js
+++ src/hooks/use-instagram.js
@@ -1,30 +1,30 @@
-// import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-// const useInstagram = () => {
-//   const data = useStaticQuery(graphql`
-//     {
-//       instagram: allInstaNode(
-//         sort: { fields: timestamp, order: DESC }
-//         limit: 18
-//       ) {
-//         nodes {
-//           caption
-//           id
-//           timestamp
-//           likes
-//           localFile {
-//             childImageSharp {
-//               fluid(quality: 100, maxWidth: 120, maxHeight: 120) {
-//                 ...GatsbyImageSharpFluid_withWebp
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
+const useInstagram = () => {
+  const data = useStaticQuery(graphql`
+    {
+      instagram: allInstaNode(
+        sort: { fields: timestamp, order: DESC }
+        limit: 18
+      ) {
+        nodes {
+          caption
+          id
+          timestamp
+          likes
+          localFile {
+            childImageSharp {
+              fluid(quality: 100, maxWidth: 120, maxHeight: 120) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
 
-//   return data.instagram.nodes
-// }
+  return data.instagram.nodes
+}
 
-// export default useInstagram
+export default useInstagram
